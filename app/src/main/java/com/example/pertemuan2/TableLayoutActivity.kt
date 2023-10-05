8166aa648542509b8bbd346977dcc73627fae558
Ilham Fauzi <ilham.fauzi.ft21@mail.umy.ac.id>
2023-10-05 20:09:32 +0700

    ilhamfauzi

---
+++ app/src/main/java/com/example/pertemuan2/TableLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pertemuan2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TableLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.table_layout)
+    }
+}
